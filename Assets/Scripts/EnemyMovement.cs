5eb6ea2c29cd18bc15a5bac5db95294b0bf47a14
Saltzus <sasu.lahtinen@outlook.com>
2024-02-01 12:34:58 +0200

    Added enemy
    -Enemy animation
    -Enemy sprite
    -Enemy movement algorythm

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5f;
+    float distance;
+    public GameObject player;
+    
+    private Animation anim;
+    private Rigidbody2D rb;
+    private bool canJump = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+
+        var step = speed * Time.deltaTime;
+
+        float distance = Vector2.Distance(gameObject.transform.position, player.transform.position);
+        Debug.Log(distance);
+        if (distance < 6)
+        {
+            anim.Play("EnemyAnimation");
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
+            Debug.Log("AMONGUUUSSSS");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
+}
