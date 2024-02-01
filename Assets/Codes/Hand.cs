a12a58057a8250c08306a516d28464afb8962160
Vili111 <vili.lahtinen1@outlook.com>
2024-02-01 11:27:00 +0200

    swordpo

--- Assets/Codes/Hand.cs
+++ Assets/Codes/Hand.cs
@@ -4,52 +4,68 @@ public class Hand : MonoBehaviour
 {
     public GameObject hand;
     public GameObject handpivot;
-    public float smooth = 200.0f;
-    private Camera mainCamera;
-    private Transform handPivotTransform;
-    public GameObject slashpf;
-    private Transform bulletSpawnPoint;
-    public int bulletSpeed;
-    private Vector3 mousePressPosition;
-    private Vector3 middlePosition;
+    public Transform spawnPoint;
+    public float chargeTime = 2.0f;
 
-    void Start()
-    {
-        mainCamera = Camera.main;
-        handPivotTransform = handpivot.transform;
-        bulletSpawnPoint = hand.transform; // Use handpivot instead of transform
-    }
+    private bool isCharging = false;
+    private float chargeTimer = 0.0f;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            mousePressPosition = hand.transform.position;
+            StartCharge();
         }
-        else if (Input.GetMouseButtonUp(0))
+
+        if (Input.GetKeyUp(KeyCode.Space))
         {
-            Vector3 mouseReleasePosition = hand.transform.position;
-            middlePosition = (mousePressPosition + mouseReleasePosition) / 2f;
-            Slash();
+            ReleaseAttack();
         }
-        else
+
+        if (isCharging)
         {
-            Vector3 mousePos = Input.mousePosition;
-            Vector3 objectPos = mainCamera.WorldToScreenPoint(transform.position);
-            mousePos.x = mousePos.x - objectPos.x;
-            mousePos.y = mousePos.y - objectPos.y;
-            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
-            handPivotTransform.rotation = Quaternion.RotateTowards(handPivotTransform.rotation, targetRotation, smooth * 100 * Time.fixedDeltaTime);
+            chargeTimer += Time.deltaTime;
+
+            if (chargeTimer >= chargeTime)
+            {
+                ReleaseAttack();
+            }
         }
+
+        // Rotate handpivot towards mouse position
+        RotateHandPivotTowardsMouse();
     }
 
-    void Slash()
+    void StartCharge()
     {
-        Vector3 worldMiddlePosition = mainCamera.ScreenToWorldPoint(new Vector3(middlePosition.x, middlePosition.y, mainCamera.nearClipPlane));
-        GameObject slash = Instantiate(slashpf, worldMiddlePosition, handPivotTransform.rotation);
-
-        Rigidbody2D slashRb = slash.GetComponent<Rigidbody2D>();
-        slashRb.velocity = slash.transform.right * bulletSpeed; // Use slash.transform.right instead of bulletSpawnPoint.forward
+        isCharging = true;
+        chargeTimer = 0.0f;
     }
+
+    void ReleaseAttack()
+    {
+        if (isCharging)
+        {
+
+            GameObject Hand = Instantiate(hand, spawnPoint.position, spawnPoint.rotation);
+            isCharging = false;
+        }
+    }
+
+    void RotateHandPivotTowardsMouse()
+    {
+        // Get the mouse position in world coordinates
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f; // Set the z-coordinate to 0, assuming your game is in 2D
+
+        // Calculate the direction towards the mouse
+        Vector3 direction = mousePosition - handpivot.transform.position;
+
+        // Calculate the angle in degrees
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Rotate handpivot towards the mouse
+        handpivot.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+
 }
