9fa44f64b2a7c3b12506510c59a69e9a480bbed1
Vili111 <vili.lahtinen1@outlook.com>
2024-02-01 13:27:06 +0200

    das

--- Assets/Scripts/swing.cs
+++ Assets/Scripts/swing.cs
@@ -1,31 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Swing : MonoBehaviour
 {
-    public int damage;
-    public GameObject swingmesh;
+    public int damageAmount = 10; // Adjust this value as needed
 
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Collision detected");
 
-    }
+        // Check if the collision is with an enemy (you might need to adjust the tag)
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collision with Enemy detected");
 
-    // Update is called once per frame
-    void Update()
-    {
+            // Apply damage to the enemy
+            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
 
-    }
-
-    // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider
-    void OnCollisionEnter(Collision collision)
-    {
-
-
-             Destroy(swingmesh);
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damageAmount);
+                Debug.Log("Dealing damage to enemy.");
+            }
+            else
+            {
+                Debug.LogWarning("Enemy does not have EnemyHealth script.");
+            }
+        }
+        else
+        {
+            Debug.Log("Collision with non-enemy object. Destroying gameObject.");
             Destroy(gameObject);
-        
+        }
     }
 }
